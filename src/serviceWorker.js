efc5c0a8d08409e85b231b9cf025195047c30aa4
mcuking <mcuking.tang@gmail.com>
2022-11-01 12:54:17 +0800

    feat: add service worker code

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,21 +1,23 @@
 import { registerRoute } from 'workbox-routing';
+import { CacheFirst } from 'workbox-strategies';
+import { ExpirationPlugin } from 'workbox-expiration';
 import Channel from '$utils/channel';
-import { busid } from '$utils/constant';
 
 const ChannelMap = new Map();
 
 registerRoute(
-  /^https?:\/\/localhost:8888\/preview\/*/,
-  async ({ request, url }) => {
-    const { href, pathname } = url;
+  /^https?:\/\/[^/]*\/([^/]{32})\/vite\/([^/]*)(\/.*)$/,
+  async ({ request, url, params }) => {
+    const [busid, wcid, pathname] = params;
+    const { href } = url;
     
     let channel = ChannelMap.get(busid);
     if (!channel) {
       channel = new Channel(busid);
       ChannelMap.set(busid, channel);
     }
-
     const res = await channel.request('serve-request', {
+      wcid,
       pathname: pathname.replace(/#.*$/, ''),
       rawUrl: href,
       accept: request?.headers?.get('accept')
@@ -34,4 +36,30 @@ registerRoute(
       statusText: 'SERVER ERROR'
     });
   }
+);
+
+registerRoute(
+  /^https:\/\/vitesandbox\.fn\..*\.com\/static\/js\//,
+  new CacheFirst({
+    cacheName: 'vite-static-root-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 day
+        maxEntries: 500
+      })
+    ]
+  })
+);
+
+registerRoute(
+  /^https:\/\/esm\.sh/,
+  new CacheFirst({ 
+    cacheName: 'vite-dependency-files-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, // 7 day
+        maxEntries: 500
+      })
+    ]
+  })
 );
